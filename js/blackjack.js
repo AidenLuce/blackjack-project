7fcb0d980e96a3a5b6f9edeeb509bda9d34ebd31
Aiden Luce <luceaiden4124@gmail.com>
2022-08-19 16:54:27 -0500

    project started

--- js/blackjack.js
+++ js/blackjack.js
@@ -1,1 +1,17 @@
-let card = Math.floor(Math.random() * (0 ,10)+1)
+// let cardType = ['hearts', 'clubs', 'diamonds', 'spades']
+// cardType = [Math.floor(Math.random()*cardType.length)]
+// let cardValue = Math.floor(Math.random() * (0 ,10)+1)
+// console.log(cardType)
+let card = {
+    value: Math.floor(Math.random() * (0 ,10)+1),
+    type:'spades',
+}
+let typeAndValue = (card.value + " of " + card.type)
+console.log(typeAndValue)
+let player = {
+
+    playersCards: [typeAndValue,typeAndValue ],
+}
+console.log(player.playersCards)
+
+let hitOrStay = confirm
